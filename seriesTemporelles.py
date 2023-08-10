c7b9e56d7d24a45c45c324e5672204949ebfec67
Utanapishtim31 <140334357+Utanapishtim31@users.noreply.github.com>
2023-08-10 17:51:56 +0200

    Update seriesTemporelles.py
    Premier jet de la description des modèles ARIMA

--- seriesTemporelles.py
+++ seriesTemporelles.py
@@ -8,3 +8,10 @@ from displayBackground import displayBackground
 def seriesTemporelles():
     displayBackground("#000000","#FFFFFF")
     st.header("Séries Temporelles")
+    st.write("L'approche par série temporelle consiste à considérer qu'il existe une corrélation entre la quantité de \
+    pluie tombée un jour donné et la quantité de pluie tombée la veille, et à exploiter cette corrélation pour réaliser des \
+    prédictions de quantité de pluie.")
+    st.markdown("Des modèles de type **ARIMA** sont entraînés pour prévoir la pluie. Par contre, ces modèles apportent plusieurs contraintes :")
+    st.markdown("*   Il ne peut y avoir qu'une seule variable prédite, ce qui nécessite d'entraîner un modèle par ville.")
+    st.markdown("*   La variable prédite doit être continue, ce qui fait que l'on va prédire la quantité de pluie et non la probabilité de pluie.")
+    
