f9ba7014f10dfa26e50f1715c30d1a25c0544f34
Pierre Le Bert <124571143+Mediapax@users.noreply.github.com>
2023-08-15 05:25:59 +0200

    Add files via upload

--- AppelResultatsKNNBokeh.py
+++ AppelResultatsKNNBokeh.py
@@ -10,4 +10,8 @@ from resultatsKNNBokeh import resultatsKNNBokeh
 # indicateurs : _v1.0.csv ==> 'accuracy_v1.0.csv', 'precision_v1.0.csv', 'recall_v1.0.csv', 'f1score_v1.0.csv', 'specificity_v1.0.csv'
 metricsUtilisees = ['l1','l2','manhattan','nan_euclidean','minkowski','chebyshev','cityblock','cosine','euclidean']
 nbVoisinsMax = 51
-resultatsKNNBokeh("accuracy_v1.0.csv", "precision_v1.0.csv", "recall_v1.0.csv", "f1score_v1.0.csv", "specificity_v1.0.csv", metricsUtilisees, nbVoisinsMax)
+poids = ['uniform','distance']
+couleurs = ['magenta','maroon','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen',
+            'mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','gold',
+            'goldenrod','navy','grey','darksalmon','red']
+resultatsKNNBokeh("accuracy_v1.0.csv", "precision_v1.0.csv", "recall_v1.0.csv", "f1score_v1.0.csv", "specificity_v1.0.csv", metricsUtilisees, nbVoisinsMax, poids, couleurs)
