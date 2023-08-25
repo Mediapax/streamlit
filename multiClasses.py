5aaa3fb2889b158334e2fae1bfbf7e213931ef8b
Utanapishtim31 <140334357+Utanapishtim31@users.noreply.github.com>
2023-08-25 11:38:01 +0200

    Update multiClasses.py
    Reduction à 2 classes

--- multiClasses.py
+++ multiClasses.py
@@ -91,13 +91,15 @@ def multiClasses():
     df_models = pd.DataFrame(list(models.keys()), columns=['id'])
     df_models['Model'] = df_models['id'].apply(lambda x: model_list[re.findall('joblib_([a-zA-Z]+)', x)[0]])
 
-    y_pred = {}
+    y_pred_train = {}
+    y_pred_test = {}
     
     for i in range(len(df_models)):
         id = filename_path+df_models.iloc[i, 0]
         pred_train = load(id+'.predtrain')
         pred_test = load(id+'.predtest')
-        y_pred[df_models['Model'].iloc[i]] = pred_test
+        y_pred_train[df_models['Model'].iloc[i]] = pred_train
+        y_pred_test[df_models['Model'].iloc[i]] = pred_test
 
         # calcul du balanced accuracy score
         df_models.loc[i, 'bal_acc_train'] = balanced_accuracy_score(y_train, pred_train)
@@ -117,34 +119,47 @@ def multiClasses():
     # modèles de regression
     with col1:
         st.markdown("**LogisticRegression**")
-        fig = ConfusionMatrixDisplay.from_predictions(y_test, y_pred['LogisticRegression'], cmap=plt.cm.Blues)
+        fig = ConfusionMatrixDisplay.from_predictions(y_test, y_pred_test['LogisticRegression'], cmap=plt.cm.Blues)
         st.pyplot(fig.figure_)
     
     # metrics
     with col2:
         st.markdown("**HistGradientBoostingRegressor**")
-        fig = ConfusionMatrixDisplay.from_predictions(y_test, y_pred['HistGradientBoostingRegressor'], cmap=plt.cm.Blues)
+        fig = ConfusionMatrixDisplay.from_predictions(y_test, y_pred_test['HistGradientBoostingRegressor'], cmap=plt.cm.Blues)
         st.pyplot(fig.figure_)
+
+    # Réduction à 2 classes
+
+    # Réduction des classes: {0, 1} -> 0, 2->1
+    truth_train = y_train // 2
+    truth_test = y_test // 2
+    
+    for i in range(len(df_models)):
+        pred_train = y_pred_train[df_models['Model'].iloc[i]] // 2
+        pred_test = y_pred_test[df_models['Model'].iloc[i]] // 2
+
+        # calcul du balanced accuracy score
+        df_models.loc[i, 'bal_acc_train'] = balanced_accuracy_score(truth_train, pred_train)
+        df_models.loc[i, 'bal_acc_test'] = balanced_accuracy_score(truth_test, pred_test)
+        # calcul du score F1
+        df_models.loc[i, 'f1_train'] = f1_score(truth_train, pred_train, average='macro')
+        df_models.loc[i, 'f1_test'] = f1_score(truth_test, pred_test, average='macro')
+
+    df_models.set_index('id', inplace=True)
+    df_models.sort_values(by='bal_acc_test', ascending=False, inplace=True)
+    st.dataframe(df_models.round(2), hide_index=True)
 
     st.header("Matrices de confusion réduite à 2 classes")
     col1, col2 = st.columns(2, gap='medium')
 
-    def reduceConfusionMatrix(cm):
-        # Réduit une matrice de confusion 3x3 en matrice de confusion 2x2
-        return np.array([[cm[:2,:2].sum(), cm[:2,2].sum()], [cm[2,:2].sum(), cm[2,2]]])
-    
     # modèles de regression
     with col1:
         st.markdown("**LogisticRegression**")
-        cm = confusion_matrix(y_test, y_pred['LogisticRegression'])
-        cm = reduceConfusionMatrix(cm)
-        fig = ConfusionMatrixDisplay(cm2, display_labels=['<1mm', '>=1mm'], cmap=plt.cm.Blues)
+        fig = ConfusionMatrixDisplay.from_predictions(truth_test, y_pred_test['LogisticRegression'] // 2, cmap=plt.cm.Blues)
         st.pyplot(fig.figure_)
     
     # metrics
     with col2:
         st.markdown("**HistGradientBoostingRegressor**")
-        cm = confusion_matrix(y_test, y_pred['HistGradientBoostingRegressor'])
-        cm = reduceConfusionMatrix(cm)
-        fig = ConfusionMatrixDisplay(cm2, display_labels=['<1mm', '>=1mm'], cmap=plt.cm.Blues)
+        fig = ConfusionMatrixDisplay.from_predictions(truth_test, y_pred_test['HistGradientBoostingRegressor'] // 2, cmap=plt.cm.Blues)
         st.pyplot(fig.figure_)
