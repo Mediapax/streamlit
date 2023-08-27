959f5b50c4621a77c2adec01af86352d3dccf4d7
Utanapishtim31 <140334357+Utanapishtim31@users.noreply.github.com>
2023-08-27 19:41:05 +0200

    Update seriesTemporelles.py

--- seriesTemporelles.py
+++ seriesTemporelles.py
@@ -6,6 +6,7 @@ from PIL import Image
 from displayBackground import displayBackground
 from joblib import load
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.metrics import balanced_accuracy_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 
