a558c14c92310fb4a2772b24a2466eeb68333c64
ArnoMac <124446323+ArnoMac@users.noreply.github.com>
2023-08-27 21:41:52 +0200

    Update regressions.py

--- regressions.py
+++ regressions.py
@@ -1,7 +1,6 @@
-# regressions
-
 import streamlit as st
 import re
+
 import pandas as pd
 import numpy as np
 from joblib import load
@@ -12,18 +11,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error, recall_score, precision_score, f1_score
 from sklearn.pipeline import Pipeline
 
-# chemin d'accès aux modèles enregistrés
-filename_path = './models/regressions/'
-
-def regressions():
+def regression():
+    st.title('Regressions') 
     
-    st.header("Régressions")
-        
     st.markdown("Une regression peut être envisagée en transformant la variable `Rainfall` en une variable `Rainfall_Tomorrow`. Il s'agira donc de prédire une quantité de pluie à venir.")
     
     
     st.subheader("Jeu de données d'entrée")
-    columns_selection=['Rainfall', 'WindGustSpeed', 'Temp9am', 'diffTempMinMax', 'diffWind3pm9am', 'diffPressure9am3pm', 'Pressure', 'DeltaP_1d', 'diffHimidity9am3pm', 'Humidity', 'DeltaH_1d']
+    columns_selection=['Rainfall', 'WindGustSpeed', 'Temp9am', 'diffTempMinMax', 'diffWind3pm9am',
+           'diffPressure9am3pm', 'Pressure', 'DeltaP_1d',
+           'diffHimidity9am3pm', 'Humidity', 'DeltaH_1d']
     st.markdown('Par soucis de simplicité et pour préserver des temps de calculs acceptable, seules les variables suivantes seront conservées en raison de leur qualité de grandeur physique:')
     st.write(columns_selection)
     st.markdown("La sélection de ces variables est aussi la résultantes de plusieurs tests qui ont montré qu'elles étaient les plus importantes dans la prédiction.")
@@ -34,17 +31,17 @@ def regressions():
     
     # modèles de regression
     with col1:
-    st.markdown("**Modèles de régressions**")
-    st.markdown("1. `LinearRegression`")
-    st.markdown('2. `HistGradientBoostingRegressor`')
-    st.markdown('3. `KNeighborsRegressor`')
+        st.markdown("**Modèles de régressions**")
+        st.markdown("1. `LinearRegression`")
+        st.markdown('2. `HistGradientBoostingRegressor`')
+        st.markdown('3. `KNeighborsRegressor`')
     
     # metrics
     with col2:
-    st.markdown("**Métriques**")
-    st.markdown("1. `r2_score`")
-    st.markdown('2. `mean_squared_error`')
-    st.markdown('3. `mean_absolute_error`')
+        st.markdown("**Métriques**")
+        st.markdown("1. `r2_score`")
+        st.markdown('2. `mean_squared_error`')
+        st.markdown('3. `mean_absolute_error`')
     
     st.divider()
     st.subheader("Transformation des données:")
@@ -52,22 +49,17 @@ def regressions():
     col1, col2 = st.columns(2, gap='medium')
     
     with col1:
-    st.markdown("**Données sources:**")
-    st.markdown("1. `StandardScaler`")
-    st.markdown('2. `PolynomialFeatures`')
-    
+        st.markdown("**Données sources:**")
+        st.markdown("1. `StandardScaler`")
+        st.markdown('2. `PolynomialFeatures`')
+        
     with col2:
-    st.markdown("**Données cibles:**")
-    st.markdown("1. Log")
-    st.markdown('2. 1/x')
+        st.markdown("**Données cibles:**")
+        st.markdown("1. Log")
+        st.markdown('2. 1/x')
     
-    # liste des modèles et noms correspondants
-    model_list = {'linreg': 'LinearRegression',
-                  'HistGradBoostingReg': 'HistGradientBoostingRegressor', 
-                  'KNNreg': 'KNeighborsRegressor'}
-    
-    ytransform_list = {'ylog1p': 'log1p',
-                       'yrecip': '1/(y+1)'}
+    # chemin d'accès aux modèles enregistrés
+    filename_path = './models/regressions/'
     
     # Résultats
     st.header("Résultats")
@@ -104,8 +96,6 @@ def regressions():
     st.markdown("A partir des résultats précédemment obtenus, nous pouvons tenter de faire une convertion en une classification binaire. Nous pouvons jouer sur les seuils afin d'optimiser un résultat comme exposé dans le graphique ci-après.")
     
     # on calcule les précisions/r2 score/recalls pour différentes valeurs de seuil pour considéré s'il pleut ou non
-    
-    
     data_visu = load(filename_path+'joblib_data_visu.dataframe')
     st.line_chart(data=data_visu, x='x', y=['recall', 'precision', 'f1_score'], width=540, height=400, use_container_width=False)
     
@@ -117,4 +107,3 @@ def regressions():
     st.markdown('Pour cette valeur, les métriques sont les suivantes:')
     
     st.write(data_visu.round(2).set_index('x').iloc[max_arg].rename('x='+str(x_opti)))
-
