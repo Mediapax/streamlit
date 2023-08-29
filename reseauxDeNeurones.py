e29865441f001e839bfd0d624087322de9727a52
Utanapishtim31 <140334357+Utanapishtim31@users.noreply.github.com>
2023-08-29 16:34:58 +0200

    Update reseauxDeNeurones.py
    Premier jet du modèle NN

--- reseauxDeNeurones.py
+++ reseauxDeNeurones.py
@@ -5,6 +5,31 @@ from resizeImage import resizeImage, loadImage
 from PIL import Image
 from displayBackground import displayBackground
 
+# chemin d'accès aux modèles enregistrés
+filename_path = './techniquesML/neural/'
+
 def reseauxDeNeurones():
     displayBackground("#000000","#FFFFFF")
     st.header("Réseaux de Neurones")
+    st.markdown("La richesse des réseaux de neurones fait qu'on peut aborder la modélisation du problème de nombreuses façons :")
+    st.markdown("- Entrée :")
+    st.markdown("    - Variables du jour")
+    st.markdown("    - Variables d'une fenêtre de *n* jours antérieurs")
+    st.markdown("- Traitement :")
+    st.markdown("    - Couches denses (réseau de neurones à propagation avant)")
+    st.markdown("    - Couches de convolution (détection de pattern)")
+    st.markdown("    - Réseau de neurones récurrents (approche temporelle)")
+    st.markdown("- Sortie :")
+    st.markdown("    - Probabilité de pluie")
+    st.markdown("    - Quantité de pluie")
+    st.markdown("Compte tenu du succès de la prédiction de pluie par le modèle ARIMA, on envisage une approche temporelle avec \
+    un réseau de neurones récurrents alimenté par une fenêtre de quelques jours de variables. En sortie, le réseau retourne \
+    la probabilité de pluie au moyen d'une fonction logistique.")
+    st.markdown("L'architecture du réseau se compose des éléments suivants :")
+    st.markdown("- En entrée : une fenêtre de 4 jours des différentes variables standardisées")
+    st.markdown("- Deux neurones récurrents de type LSTM en cascade")
+    st.markdown("- Deux couches denses")
+    st.markdown("- En sortie : un unique neurone avec activation sigmoïde retournant la probabilité de pluie")
+    st.markdown("- Des couches de *dropout* sont intercalées au niveau des couches denses")
+    st.subheader("Architecture du réseau")
+    st.image(loadImage(filename_path+"lstm_model_graph.png",400))
