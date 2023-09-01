3128e16038c8dc9f739bef2a6d280d1560829246
Pierre Le Bert <124571143+Mediapax@users.noreply.github.com>
2023-09-01 21:20:28 +0200

    Add files via upload

--- displayBranchingOut.py
+++ displayBranchingOut.py
@@ -1,8 +1,10 @@
 # displayBranchingOut
 
 import streamlit as st
+from resizeImage import resizeImage, loadImage
 
 def displayBranchingOut():
+    chemin = ".\\photosequipe\\"
     st.title("Bilan et suite du projet")
     st.write("")
     st.write("Le tableau suivant résume l’ordre de grandeur des meilleurs résultats que nous avons obtenus:")
@@ -32,25 +34,60 @@ def displayBranchingOut():
         "</table>"
     st.markdown(meilleursResultats,unsafe_allow_html = True)
     st.write("")
-    st.write("Il est intéressant de noter qu’avec quelques optimisations, nous avons chacun réussi à obtenir des scores similaires malgré"\
-             " des méthodes de simulation très différentes.")
-    st.write("")
-    st.write("Nos résultats montrent qu’avec du machine learning, nous réussissons à améliorer la prédiction par rapport à des modèles naïfs."\
-             " Cependant, il semble malgré tout extrêmement compliqué de prédire avec précision la pluie à J+1 avec le jeu de données dont on dispose.")
-    st.write("")
-    st.write("Si GrouseLager souhaite utiliser nos modèles de prédiction, l’entreprise devra le faire en gardant à l’esprit que l’indice de confiance"\
+
+    col1, col2 = st.columns([0.2, 0.7])
+    with col1 :
+        imgarnaud = loadImage(chemin + "arnaud150.jpg", 150)
+        st.image(imgarnaud)
+        st.caption("Arnaud")
+    
+    with col2:
+        st.write("")
+        st.write("")
+        st.write("*Il est intéressant de noter qu’avec quelques optimisations, nous avons chacun réussi à obtenir des scores similaires malgré"\
+             " des méthodes de simulation très différentes.*")
+
+    col3, col4 = st.columns([0.7, 0.2])
+    with col3 :
+        st.write("")
+        st.write("")
+        st.write("*Nos résultats montrent qu’avec du machine learning, nous réussissons à améliorer la prédiction par rapport à des modèles naïfs."\
+             " Cependant, il semble malgré tout extrêmement compliqué de prédire avec précision la pluie à J+1 avec le jeu de données dont on dispose.*")
+    with col4:
+        imgpierre = loadImage(chemin + "pierre150.jpg", 150)
+        st.image(imgpierre)
+        st.caption("Pierre")
+
+    col5, col6 = st.columns([0.2, 0.7])
+    with col5 :
+        imgolivier = loadImage(chemin + "olivier150.jpg", 150)
+        st.image(imgolivier)
+        st.caption("Olivier")
+        
+    with col6:
+        st.write("")
+        st.write("*Nos résultats sont malgré tout encourageants pour 2 raisons principales:*")
+        st.write("*-	Nous avons réalisé ces études avec très peu de ressources*")
+        st.write("*-	Les modélisations ont déjà permis -pour certaines villes- d’apporter des prédictions que nous jugeons utilisables.*")
+        
+
+    st.divider()
+    st.write()
+    
+    st.write("Si **GrouseLager** *(notre hypothétique brasseur Australien)* souhaite utiliser nos modèles de prédiction, l’entreprise devra le faire en gardant à l’esprit que l’indice de confiance"\
              " est faible. Il doit s’agir là d’un outil utilisé en complément d’autres processus de décision. Cet outil ne se suffit pas à lui-même "\
                 "pour prédire la pluie.")
+    imgbeer = loadImage("AustralianBeer.jpg",1045)
+    st.image(imgbeer)
     st.write("")
-    st.write("**Nos résultats sont malgré tout encourageants pour 2 raisons principales:**")
-    st.write("-	Nous avons réalisé ces études avec très peu de ressources")
-    st.write("-	Les modélisations ont déjà permis -pour certaines villes- d’apporter des prédictions que nous jugeons utilisables.")
+    st.divider()
     st.write("")
     st.write("**Pour aller plus loin, nous recommandons de financer:**")
     st.write("-	l’acquisition de bases de données météorologiques plus fournies en données exploitables, car cette base est notoirement lacunaire sur"\
              " certains types de données et les données présentes y sont extrêmement basiques.")
     st.write("-	des ressources matérielles conséquentes afin de pouvoir entrainer des modèles très complexes.")
     st.write("")
+    st.divider()
     st.write("**Exemples de besoins permettant d’améliorer nos prédictions :**")
     st.write("-	Disposer d’un maillage bien plus conséquent de stations météorologiques. En effet, pour couvrir toutes l’Australie, nos données"\
              " ne proposent que 49 sites. De plus, ces sites sont très mal répartis sur le territoire.")
@@ -63,8 +100,18 @@ def displayBranchingOut():
              " semblaient assez bien corrélées à la variable cible; en particuliers la  variable “nébulosité totale”. De plus, la connaissance du type"\
                  " de nuages et de leur altitude estimée pourrait nous apporter de précieuses informations manquantes.")
     st.write("")
-    st.write("**Nous pourrions acquérir différents types de relevés sur la couverture nuageuse, vus du sol mais avec un découpage à minima en 3 couches,"\
-             " avec par exemple :**")
+    st.divider()
+    st.write("")
+    
+    col7, col8 = st.columns([0.7, 0.3])
+    with col7 :
+        st.write("**Nous pourrions acquérir différents types de relevés sur la couverture nuageuse, vus du sol mais avec un découpage à minima en 3 couches,"\
+                " avec par exemple :**")
+        
+    with col8:
+        imgdata = loadImage("datagouvfr.jpg",300)
+        st.image(imgdata)
+        
     st.write("- nébulosité des nuages de l’étage inférieur,")
     st.write("- hauteur de la base des nuages de l’étage inférieur,")
     st.write("- nébulosité de la couche inférieure,")
