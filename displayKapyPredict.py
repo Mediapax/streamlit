30df6c0658784da5ee5b9d61fbe7274189f046f9
Utanapishtim31 <140334357+Utanapishtim31@users.noreply.github.com>
2023-09-01 13:03:36 +0200

    Update displayKapyPredict.py

--- displayKapyPredict.py
+++ displayKapyPredict.py
@@ -137,8 +137,13 @@ def displayKapyPredict():
         X_offset_idx = [i + '_offset' for i in lin_idx]
         X_scale_idx = [i + '_scale' for i in lin_idx]
         lin = model_params[lin_idx].to_numpy()
+        lin_offset = model_params[X_offset_idx].to_numpy()
+        lin_scale = model_params[X_scale_idx].to_numpy()
         log_rainfall = np.log1p(X_new.iloc[0, 0])
-        X_scaled = (X_new.iloc[1:, 0].to_numpy() + model_params[X_offset_idx].to_numpy()) * model_params[X_scale_idx].to_numpy()
+        st.write(lin_offset)
+        st.write(lin_scale)
+        st.write(X_new.iloc[1:, 0])
+        X_scaled = (X_new.iloc[1:, 0].to_numpy() + lin_offset) * lin_scale
         st.write(X_scaled)
         y_pred = np.sum(lin * X_scaled)
         y_pred += cst
